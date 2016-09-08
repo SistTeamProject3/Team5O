fa5c0b20e4b0fe1f6e111ea9fa23a3ebfc4bcf6b
조성환 <jossi89@hanmail.net>
2016-09-08 17:45:34 +0900

    아ㅏㅏ 짜증난다 세션 미완성.

--- src/sist/co/controller/MemBerController.java
+++ src/sist/co/controller/MemBerController.java
@@ -2,17 +2,30 @@ package sist.co.controller;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import sist.co.model.MemberDTO;
+import sist.co.model.MsgMember;
+import sist.co.service.MemberService;
+
 
 @Controller
 public class MemBerController {
 
 	private static final Logger logger = LoggerFactory.getLogger(MemBerController.class);
+	
+	@Autowired
+	MemberService MemberService;
 	
 	
 	@RequestMapping(value="login.do", method=RequestMethod.GET)
@@ -31,5 +44,36 @@ public class MemBerController {
 		return "regi.tiles";
 	}
 	
+	
+	@RequestMapping(value="check_member.do", method=RequestMethod.POST)
+	@ResponseBody
+	public MsgMember check_member( HttpServletRequest request, MemberDTO member, Model model) throws Exception{
+
+		
+		logger.info("MemberController getID " + new Date());	
+		
+		int count= MemberService.check_member(member);
+		int random = (int)(Math.random()*10000000)+10000000;
+		System.out.println("random = "+random);
+		
+		request.getSession().setAttribute("random", random);
+		/*model.addAttribute("random",random);*/
+		MsgMember msg = new MsgMember();
+		if(count > 0){
+			
+			msg.setMessage("Sucs");
+			
+					
+		}else{
+			
+			msg.setMessage("Fail");
+			System.out.println("들어옴???");
+			System.out.println("들어옴???");
+			System.out.println("들어옴???");
+			
+		}
+		return msg;
+		
+	}
 	
 }
--- src/sist/co/service/MemberService.java
+++ src/sist/co/service/MemberService.java
@@ -1,8 +1,10 @@
 package sist.co.service;
 
-
+import sist.co.model.MemberDTO;
 
 public interface MemberService {
+
+	int check_member(MemberDTO member)throws Exception;
 
 
 }
