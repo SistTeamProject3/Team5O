6bda66eb03b781a71aa77cdb71dda73864bd71e8
조용호 <popoky2@naver.com>
2016-09-09 11:40:01 +0900

    병합 전

--- target/classes/aysql.sql
+++ target/classes/aysql.sql
@@ -1,4 +1,6 @@
--- [ 회원 ] --
+-- (0908수정할점)모든 DB명, seq 앞에 'TP3_' 붙히기
+
+-- [1] member
 
 DROP TABLE TP3_MEMBER
 CASCADE CONSTRAINTS;
@@ -22,47 +24,61 @@ CREATE TABLE TP3_MEMBER(
 	M_DEACTIVATION	NUMBER(1)		NOT NULL,
 	M_ASK			NUMBER(1)		NOT NULL,
 	M_CONTENT		VARCHAR2(100),
-	M_STOPDATE		VARCHAR2(8),
-	M_GENDER		NUMBER(1)		NOT NULL
+	M_STOPDATE		VARCHAR2(8)
 );
 
 
 insert into TP3_MEMBER
 values('qwer', 'qwer', 'qwer', 'qwer', '01012345678', sysdate, '881111',
-'', '', '', '', 0, '서울시', 0, 0, 'qwer입니다', '',1);
+'', '', '', '', 0, '서울시', 0, 0, 'qwer입니다', '');
 insert into TP3_MEMBER
 values('asdf', 'asdf', 'asdf', 'asdf', '01056471234', sysdate, '111111',
-'', '', '', '', 0, '서울시', 0,  0, 'asdf입니다', '',2);
+'', '', '', '', 0, '서울시', 0,  0, 'asdf입니다', '');
 insert into TP3_MEMBER
 values('kwon', 'kwon', 'kwon', 'kwon', '01001001000', sysdate, '111111',
-'', '', '', '', 0, '서울시', 0,  0, 'kwon입니다', '',1);
+'', '', '', '', 0, '서울시', 0,  0, 'kwon입니다', '');
 insert into TP3_MEMBER
 values('kim', 'kim', 'kim', 'kim', '01078945612', sysdate, '111111',
-'', '', '', '', 0, '서울시', 0,  0, 'kim입니다', '',1);
+'', '', '', '', 0, '서울시', 0,  0, 'kim입니다', '');
 insert into TP3_MEMBER
 values('young', 'young', 'young', 'young', '01088885252', sysdate, '111111',
-'', '', '', '', 0, '서울시', 0,  0, 'young입니다', '',1);
+'', '', '', '', 0, '서울시', 0,  0, 'young입니다', '');
 insert into TP3_MEMBER
 values('Ahyo', 'Ahyo', 'Ahyo', 'Ahyo', '01012341234', sysdate, '111111',
-'', '', '', '', 0, '서울시', 0,  0, 'Ahyo입니다', '',1);
+'', '', '', '', 0, '서울시', 0,  0, 'Ahyo입니다', '');
 insert into TP3_MEMBER
 values('nanna', 'nanna', 'nanna', 'nanna', '01012341234', sysdate, '111111',
-'', '', '', '', 0, '경기도', 0,  0, 'nanna입니다', '',1);
+'', '', '', '', 0, '경기도', 0,  0, 'nanna입니다', '');
 insert into TP3_MEMBER
 values('imma', 'imma', 'imma', 'imma', '01012341234', sysdate, '111111',
-'', '', '', '', 0, '경기도', 0,  0, 'imma입니다', '',1);
+'', '', '', '', 0, '경기도', 0,  0, 'imma입니다', '');
 insert into TP3_MEMBER
 values('yonyon', 'yonyon', 'yonyon', 'yonyon', '01012341234', sysdate, '111111',
-'', '', '', '', 0, '경기도', 0,  0, 'yonyon입니다', '',1);
+'', '', '', '', 0, '경기도', 0,  0, 'yonyon입니다', '');
 insert into TP3_MEMBER
 values('asktest1', 'asktest1', 'asktest1', 'asktest1', '01012341234', sysdate, '111111',
-'', '', '', '', 0, '경기도', 0,  0, 'asktest1입니다', '',1);
+'', '', '', '', 0, '경기도', 0,  0, 'asktest1입니다', '');
 insert into TP3_MEMBER
 values('asktest2', 'asktest2', 'asktest2', 'asktest2', '01012341234', sysdate, '111111',
-'', '', '', '', 0, '경기도', 0,  0, 'asktest2입니다', '',1);
+'', '', '', '', 0, '경기도', 0,  0, 'asktest2입니다', '');
 
 
--- [ 친구 ] --
+
+update TP3_MEMBER set m_profile='1473138776001.jpg'
+where m_id='imma';
+update TP3_MEMBER set m_profile='IMG_3454.JPG'
+where m_id='yonyon';
+update TP3_MEMBER set m_profile='1.jpg'
+where m_id='asktest1';
+update TP3_MEMBER set m_profile='2.jpg'
+where m_id='asktest2';
+update TP3_MEMBER set m_profile='3.jpg'
+where m_id='Ahyo';
+update TP3_MEMBER set m_profile='4.jpg'
+where m_id='young';
+
+
+-- [2] friend
 
 DROP TABLE TP3_FRIEND
 CASCADE CONSTRAINT;
@@ -115,50 +131,14 @@ values(TP3_FRIEND_SEQ.nextval, 'qwer', 'asktest1',  0, 2, 0);
 insert into TP3_FRIEND
 values(TP3_FRIEND_SEQ.nextval, 'qwer', 'asktest2', 0, 2, 0);
 
+update FRIEND set f_block=1 where m_id='qwer' and f_id='kwon';
+update FRIEND set f_block=1 where m_id='qwer' and f_id='Ahyo';
 
--- [ 이벤트 ] --
+SELECT COUNT(*) FROM TP3_FRIEND
+ 	WHERE M_ID='qwer'
 
-DROP TABLE TP3_EVENT
-CASCADE CONSTRAINTS;
-
-DROP SEQUENCE TP3_EVENT_SEQ;
-
-CREATE TABLE TP3_EVENT(
-	E_SEQ			NUMBER,
-	E_TYPE			NUMBER(1)		NOT NULL,
-	G_SEQ			NUMBER,
-	M_NICKNAME		VARCHAR2(20),
-	E_TITLE			VARCHAR2(100)	NOT NULL,
-	E_CONTENT		VARCHAR2(4000)	NOT NULL,
-	E_LOCATION		VARCHAR2(100),
-	E_START_DATE	VARCHAR2(8)		NOT NULL,
-	E_END_DATE		VARCHAR2(8),
-	E_WRITE_DATE	DATE			NOT NULL,
-	E_IMAGE			VARCHAR2(1000),
-	CONSTRAINT TP3_EVENT_PK PRIMARY KEY(E_SEQ)
---	CONSTRAINT TP3_EVENT_FK_1 FOREIGN KEY(G_SEQ) REFERENCES TP3_GROUP(xxx)
---	CONSTRAINT TP3_EVENT_FK_2 FOREIGN KEY(M_NICKNAME) REFERENCES TP3_MEMBER(xxx)
-);
-
-CREATE SEQUENCE TP3_EVENT_SEQ
-START WITH 1 INCREMENT BY 1;
-
--- [ 그룹 ] --
-
-CREATE TABLE TP3_MAKEGROUP(
-g_seq NUMBER PRIMARY KEY,
-g_name VARCHAR2(100) NOT NULL,
-g_photo	VARCHAR2(1000),
-g_type NUMBER(1) NOT NULL,
-g_manager VARCHAR2(100) NOT NULL
-);
-
-CREATE TABLE TP3_GROUPMEMBER(
-g_seq NUMBER NOT NULL,
-m_id VARCHAR2(100) NOT NULL,
-g_auth NUMBER(1) NOT NULL
-);
-
-CREATE SEQUENCE TP3_GROUP_SEQ START WITH 1 INCREMENT BY 1;
-
-
+SELECT * FROM TP3_FRIEND 
+ 	WHERE M_ID='qwer' AND F_ACCEPT>0 AND F_BLOCK=0
+update TP3_FRIEND_SEQ set f_accept=2
+where TP3_FRIEND_SEQ=4
+update TP3_FRIEND_SEQ SET F_GROUP=0 WHERE M_ID='qwer' AND F_ID='asdf'
